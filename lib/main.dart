144f821463eb5c49673199b4040ad2d3b9fb413f
lesleyferreira <lesley.ferreira.tw@naturapay.net>
2024-04-23 12:19:35 -0300

    chore: cria tela de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,152 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  MyApp myapp = const MyApp();
+  runApp(myapp);
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  static const TextStyle textButtonStyle = TextStyle(color: Colors.white);
+  get textFormFieldBorder => OutlineInputBorder(
+    borderRadius: BorderRadius.circular(5.0),
+    borderSide: const BorderSide(),
+  );
+
+  TextStyle textStyle({bool applyBold = false}) {
+    TextStyle textStyle;
+    if (applyBold) {
+      textStyle = const TextStyle(fontWeight: FontWeight.bold);
+    } else {
+      textStyle = const TextStyle();
+    }
+    return textStyle;
+  }
+  Column formItem({required String text, String? labelText, bool applyBold = false}) {
+    List <Widget> children = <Widget>[
+      Padding(
+        padding: const EdgeInsets.only(),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+              text,
+              style: textStyle(applyBold: applyBold)
+          ),
+        ),
+      ),
+
+    ];
+    if (labelText != null) {
+      children.add(
+        Padding(
+          padding: const EdgeInsets.only(),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: labelText,
+                border: textFormFieldBorder,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: children,
+    );
+  }
+
+  get buttonStyle => ButtonStyle(
+    elevation: MaterialStateProperty.all(5),
+    backgroundColor: MaterialStateProperty.all(
+      Colors.blue.shade400,
+    ),
+    shape: MaterialStateProperty.all(
+      RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Exercício Montagem de GUI',
+            style: textButtonStyle,
+          ),
+          backgroundColor: Colors.blue.shade400,
+        ),
+        body: Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24.0,
+              vertical: 24.0,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            child: Form(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: formItem(text: 'TELA DE CADASTRO', applyBold: true),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 24.0),
+                    child: formItem(text: 'NOME:', labelText: 'Digite o nome'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 24.0),
+                    child: formItem(text: 'ENDEREÇO:', labelText: 'Digite o endereço'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 24.0),
+                    child: formItem(text: 'EMAIL:', labelText: 'Digite o email'),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(top: 24.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          style: buttonStyle,
+                          child: const Text(
+                            'CANCELAR',
+                            style: textButtonStyle,
+                          ),
+                          onPressed: () {},
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 16.0,
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: buttonStyle,
+                          child: const Text(
+                            'SALVAR',
+                            style: textButtonStyle,
+                          ),
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
